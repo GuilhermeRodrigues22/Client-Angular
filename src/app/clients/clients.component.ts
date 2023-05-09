8bdac0cb331b3dda35bc9ac106305b236da2a967
GuilhermeRodrigues22 <guicordeiro556@gmail.com>
2023-05-09 14:49:50 -0300

    ajustes

--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -2,26 +2,36 @@ import { Observable } from 'rxjs';
 import { ClientService } from './../client.service';
 import { Component, OnInit } from '@angular/core';
 import { Client } from '../client';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
-  styleUrls: ['./clients.component.css']
+  styleUrls: ['./clients.component.css'],
 })
-export class ClientsComponent implements OnInit{
+export class ClientsComponent implements OnInit {
+  clients: Client[] = [];
 
-  clients: Client [] = [];
+  formGroupClient: FormGroup;
 
-  constructor (private ClientService: ClientService){}
+  constructor(
+    private ClientService: ClientService,
+    private formBuilder: FormBuilder
+  ) {
+    this.formGroupClient = formBuilder.group({
+      id: [''],
+      name: [''],
+      email: ['']
+    });
+  }
+
   ngOnInit(): void {
     this.loadClients();
   }
 
   loadClients() {
-   this.ClientService.getClients().subscribe( {
-    next : data => this.clients = data
-   });
+    this.ClientService.getClients().subscribe({
+      next: (data) => (this.clients = data),
+    });
   }
-
-
 }
